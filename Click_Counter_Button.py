0eac29ba18a5d5ae5719985a0e12ac2dd65fe738
Max <maxcodingtx@gmail.com>
2023-05-17 10:43:27 -0500

    Create all buttons and labels needed
    Created the button which will be clicked on. Created 2 labels, one of which will point to the other label which will be showing the amount of times the button has been clicked

--- Click_Counter_Button.py
+++ Click_Counter_Button.py
@@ -8,4 +8,20 @@ app.geometry("800x600")
 app.title("Click Counter Button")
 
 
+
+
+click_button = tk.Button(app, text="Click Here", font=('arial', 18), bg='white', height=7, width=20, command="")
+click_button.place(relx=.5, rely=.33, anchor='center')
+
+
+click_label_count = tk.Label(app, text="Clicks on button\n ↓", font=("arial", 18), bg='white',)
+click_label_count.place(relx=.5, rely=.66, anchor='center')
+
+
+click_count = tk.Label(app, font=('arial', 12), bg='white')
+click_count.place(relx=.5,rely=.75,anchor='center')
+
+
+
+
 app.mainloop()
