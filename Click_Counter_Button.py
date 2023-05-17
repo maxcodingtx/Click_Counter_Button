b377cbb569939b56199239ca9f865147e8da3c6e
Max <maxcodingtx@gmail.com>
2023-05-17 10:35:04 -0500

    Set up the project basics
    I set up all the project basics. This includes the window, size of window, and name of window.

--- Click_Counter_Button.py
+++ Click_Counter_Button.py
@@ -1,3 +1,11 @@
 
 #Click_Counter_Button - This program creates a button that tracks the amount of times you have clicked on it
 
+import tkinter as tk
+
+app = tk.Tk()
+app.geometry("800x600")
+app.title("Click Counter Button")
+
+
+app.mainloop()
